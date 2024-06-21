90cbfaf433491f28fe0ee56a01b9fdada3bd7b17
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-06-21 19:54:15 +0300

    setup graphql with basic user and tracnsaction resolver and typedefs

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  rules: {
+    // Add any custom rules here
+  },
+};
