dd3dbd1d7f2b76489d21febfd108cc564e02d3bb
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-06-22 23:35:32 +0300

    Design the frontend UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
-function App() {
-  return <div className="text-3xl font-bold underline">Hello, World!</div>;
-}
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/ui/Header";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import TransactionPage from "./pages/TransactionPage";
+import NotFound from "./pages/NotFound";
+import HomePage from "./pages/HomePage";
 
+function App() {
+  const authUser = true;
+  return (
+    <>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
 export default App;
